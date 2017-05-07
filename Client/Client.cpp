155b82031821caa6b44f5ea1cb342bd518cc34ab
quanvuhust <quan0095@gmail.com>
2017-05-07 13:34:08 +0700

    Gộp hàm Send() với Receive() vào làm một. Thêm hàm GetIPServer() cho Client. Để có thể đặt IP của Server mặc định là "localhost".  Nếu muốn dùng IP khác thì dùng hàm GetIPServer(). Thêm macro debug in ra màn hình khi Constructor được gọi.

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -21,32 +21,27 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-	if (argc != 2) {
-		argv[1] = "localhost";
+	Client myClient;
+	
+	if (argc == 2) {
+		myClient.GetIPServer(argv[1]);
 	}
-
-	Client myClient(argv[1]);
 
 	myClient.Connect();
 
 	system("cls");
-	cout << "Connecting is success" << endl;
+	cout<< "Connecting is success"<< endl;
 
-	HANDLE h[2];
-	h[0] = myClient.Send();
-	h[1] = myClient.Receive();
-
-	WaitForMultipleObjects(2, h, false, INFINITE);
-
-	CloseHandle(h[0]);
-	CloseHandle(h[1]);
+	myClient.SendAndReceive();
+	cout << "Connection closing...\n";
 
 	myClient.CloseConnect();
 	return 0;
 }
 
-Client::Client(const char* IP_ADDRESS) :ConnectSocket(INVALID_SOCKET), result(NULL), ptr(NULL)
+Client::Client(void):ConnectSocket(INVALID_SOCKET), result(NULL), ptr(NULL), IP_ADDRESS("localhost")
 {
+	DEBUG_CONSTRUCTOR
 	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
 
 	if (iResult != 0) {
@@ -59,7 +54,21 @@ Client::Client(const char* IP_ADDRESS) :ConnectSocket(INVALID_SOCKET), result(NU
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_protocol = IPPROTO_TCP;
+}
 
+Client::~Client(void)
+{
+	DEBUG_DESTRUCTOR
+	closesocket(ConnectSocket);
+	WSACleanup();
+}
+
+void Client::GetIPServer(char* IP_ADDRESS)
+{
+	this->IP_ADDRESS = IP_ADDRESS;
+}
+
+void Client::Connect() {
 	// Resolve the server address and port
 	iResult = getaddrinfo(IP_ADDRESS, DEFAULT_PORT, &hints, &result);
 	if (iResult != 0) {
@@ -67,21 +76,13 @@ Client::Client(const char* IP_ADDRESS) :ConnectSocket(INVALID_SOCKET), result(NU
 		this->~Client();
 		exit(1);
 	}
-}
 
-Client::~Client(void)
-{
-	closesocket(ConnectSocket);
-	WSACleanup();
-}
-
-void Client::Connect() {
 	for (ptr = result; ptr != NULL;ptr = ptr->ai_next) {
 		// Create a SOCKET for connecting to server
 		ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
 
 		if (ConnectSocket == INVALID_SOCKET) {
-			cerr << "socket failed with error: " << WSAGetLastError();
+			cerr<<"socket failed with error: "<< WSAGetLastError();
 			this->~Client();
 			exit(1);
 		}
@@ -99,7 +100,7 @@ void Client::Connect() {
 	freeaddrinfo(result);
 
 	if (ConnectSocket == INVALID_SOCKET) {
-		cerr << "Unable to connect to server!\n";
+		cerr<< "Unable to connect to server!\n";
 		this->~Client();
 		exit(1);
 	}
@@ -120,7 +121,7 @@ void SendData(Client *myClient)
 	do {
 		// Send an initial buffer
 		cin.getline(sendbuf, DEFAULT_BUFLEN);
-
+		
 		myClient->iResult = send(myClient->ConnectSocket, sendbuf, (int)strlen(sendbuf) + 1, 0);
 		if (myClient->iResult == SOCKET_ERROR) {
 			cerr << "send failed with error: " << WSAGetLastError() << endl;
@@ -135,14 +136,18 @@ void SendData(Client *myClient)
 	} while (1);
 }
 
-HANDLE Client::Send(void)
+void Client::SendAndReceive(void)
 {
-	HANDLE h;
+	HANDLE h[2];
 	DWORD id;
 
-	h = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)SendData, this, FALSE, &id);
+	h[0] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)SendData, this, FALSE, &id);
+	h[1] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)ReceiveData, this, FALSE, &id);
 
-	return h;
+	WaitForMultipleObjects(2, h, false, INFINITE);
+
+	CloseHandle(h[0]);
+	CloseHandle(h[1]);
 }
 
 void ReceiveData(Client *myClient)
@@ -162,28 +167,14 @@ void ReceiveData(Client *myClient)
 		if (myClient->iResult > 0)
 		{
 			cout << "->\t\t" << recvbuf << endl;
-		}
-		else if (myClient->iResult == 0)
-		{
-			cout << "Connection closed\n";
-		}
-		else
+		}	
+		else if (myClient->iResult < 0)
 		{
 			cerr << "recv failed with error: " << WSAGetLastError() << endl;
 			myClient->~Client();
 			exit(1);
 		}
 	} while (myClient->iResult > 0);
-}
-
-HANDLE Client::Receive(void)
-{
-	HANDLE h;
-	DWORD id;
-
-	h = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)ReceiveData, this, FALSE, &id);
-
-	return h;
 }
 
 void Client::CloseConnect(void)
